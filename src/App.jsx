6d122866cc37842b121b2cf1dcc7fb009d104357
Bonnie Wall <bonniew@missionreadyhq.com>
2024-12-10 20:17:06 +1300

    Added an h3

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ function App() {
     <>
       <h1>This will be deployed</h1>
       <h2 style={{backgroundColor: "deeppink", color: "deepskyblue"}}>This has been added after the Netlify Deploy</h2>
+      <h3 style={{backgroundColor: "chartreuse", color: "black"}}>This is now also deployed on Vercel</h3>
     </>
   )
 }
